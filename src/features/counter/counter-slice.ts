3cebc7c6da6c1ef821faec31f8a9b7778af0ff2c
Md. Shofiqul Islam <shofiqebr@gmail.com>
2024-01-27 16:10:07 +0600

    added counter page

---
+++ src/features/counter/counter-slice.ts
@@ -0,0 +1,23 @@
+// Ducks pattern
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+    value:number;
+}
+
+const initialState: CounterState ={
+    value: 0,
+}
+
+const counterSlice = createSlice({
+    name: 'counter',
+    initialState,
+    reducers: {
+        increment(state){
+            state.value++;
+        },
+    },
+});
+
+export const {increment} = counterSlice.actions;
+export default counterSlice.reducer;
